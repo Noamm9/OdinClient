9d4a002f6ce9d6922e4b09697811d1189a56d586
Noamm9 <belkunsama@gmail.com>
2026-09-05 23:01:45 +0300

    check invulnerableTicks after drawing box

--- src/main/kotlin/foo/starred/odinclient/features/impl/dungeons/Highlight.kt
+++ src/main/kotlin/foo/starred/odinclient/features/impl/dungeons/Highlight.kt
@@ -155,8 +155,8 @@ object Highlight : Module(
             witherIds.forEach { id ->
                 val entity = world.getEntity(id) as? WitherBoss ?: return@forEach
                 if (!entity.isAlive) return@forEach
-                if (entity.invulnerableTicks == 0) return@forEach
                 drawStyledBox(entity.renderBoundingBox, witherColor, renderStyle, depthCheck)
+                if (entity.invulnerableTicks == 0) return@forEach
                 if (bool1) drawTracer(entity.renderPos, witherColor, depth = depthCheck)
             }
 
